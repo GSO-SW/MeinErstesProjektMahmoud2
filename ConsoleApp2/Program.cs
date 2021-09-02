597d09624e02817942ba82a3770bda8d26832879
Mahmoud Khodrawi <mahmoud.k2@gso.schule.koeln>
2021-09-02 10:47:19 +0200

    Mahmoud

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,11 +6,9 @@ namespace ConsoleApp2
     {
         static void Main()
         {
-         Console.ReadKey();
 
          
-
-         int value = 23;
+         int value = 1234;
 
          double value2 = 1000.11;
          Console.WriteLine(value + value2);
@@ -20,9 +18,22 @@ namespace ConsoleApp2
          Console.WriteLine
                 ("Wert von 'a' = {0}\nWert von 'b' = {1}", a, b);
 
-         Console.WriteLine("Ich kauf {0.,3} Eier", a);
+         Console.WriteLine("Ich kaufe {0,10} Eier", a);
+         Console.WriteLine("Ich kaufe {0,-10} Eier", a);
+
+         Console.WriteLine("value= {0:E}", value);
+         Console.WriteLine("value= {0:E2}", value);
+
+         float value3 = 0.2512F;
+         Console.WriteLine("value3={0,10:G}", value3);
+         Console.WriteLine("value3={0:P4}", value3);
+         Console.WriteLine($"a={a},b={b}");
+         Console.WriteLine("a={0},b={1}", a, b);
+                 
 
 
+
+         Console.ReadKey();
 
 
 
