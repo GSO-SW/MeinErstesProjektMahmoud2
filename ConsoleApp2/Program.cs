2c701c31c6fd2306b672f163a10a9cd031c78c60
Mahmoud Khodrawi <mahmoud.k2@gso.schule.koeln>
2021-09-02 09:55:04 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hallo");
+
+            Console.ReadLine();
+
+            Console.ReadKey();
+
+            int value;
+
+            value = 23;
+
+            double value2 = 1000.11;
+            Console.WriteLine(value + value2);
+
+            long a = 10, b = 1000000000000000000;
+
+            Console.WriteLine
+                ("Wert von 'a' = {0}\nWert von 'b' = {1}", b, a);
+
+
+
+
+
+        }
+    }
+}
